f8fc0cd557b3986cf175e4adb22e190cc10bf289
Carlo Leonardi <carloleonardi83@gmail.com>
2018-06-01 16:11:38 +0200

    addgin shape

--- src/app/shape/shape.component.ts
+++ src/app/shape/shape.component.ts
@@ -1,3 +1,7 @@
+import { Circle } from '../Circle';
+import { Shape } from '../Shape';
+import { Triangle } from '../Triangle';
+import { Parallelogram, Square } from '../Parallelogram';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,11 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shape.component.css']
 })
 export class ShapeComponent implements OnInit {
+  public currentShapeValue: string  = "square";
+  public param1: number = 0;
+  public param2: number = 0;
+  public currentShape: Shape;
 
   constructor() { }
 
   ngOnInit() {
     console.log("ciao");
+    let circle: Circle = new Circle(3);
+    console.log("circle", circle.getArea());
+    console.log("circle", circle.getPerimeter());
+    this.currentShape = new Square(this.param1);
+  }
+
+  generate(){
+    if (this.currentShapeValue === 'triangle') {
+        this.currentShape = new Triangle(this.param1, this.param2);
+    } else if (this.currentShapeValue === 'square'){
+      this.currentShape = new Square(this.param1);
+    } else if (this.currentShapeValue === 'circle'){
+      this.currentShape = new Circle(this.param1);
+    } else {
+      this.currentShape = new Parallelogram(this.param1, this.param2); 
+    }
+  }
+
+  setShape(newShape: string):void{
+    this.currentShapeValue = newShape;
+    console.log("this.currentShape:", this.currentShapeValue);
   }
 
 }
